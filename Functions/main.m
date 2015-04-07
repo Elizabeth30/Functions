f75f104ec1cee1b33e4b4ccca5cbf443bb602812
Elizabeth <ElizabethSanchez@crbc.net>
2015-04-07 10:05:49 -0400

    Functions

--- Functions/main.m
+++ Functions/main.m
@@ -8,10 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+int square (int value){
+    return value*value;
+}
+int addInt (int value, int value2, int value3){
+    return value+value2+value3;
+}
+float addFloat(float value, float value2, float value3){
+    return value+value2+value3;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"%f", addFloat (10.5,5.5,20.5));
     }
+    
     return 0;
+
 }
